e23309747502556fb6f3699527711c5ffe23a696
muhammaddaffafauzan <mobileasia215@gmail.com>
2024-05-03 21:03:32 +0700

    update!

--- index.js
+++ index.js
@@ -25,22 +25,26 @@ app.get("/api/hello", function (req, res) {
 let urlDatabase = {};
 let shortUrlCounter = 1;
 
+// Endpoint to handle URL shortening
 app.post("/api/shorturl", function (req, res) {
   const originalUrl = req.body.url;
 
-  // Validation regex for URL format
+  // Validation regex for URL format (accepts only HTTP)
   const urlRegex = /^http:\/\/(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:\/\S*)?$/;
 
   if (!urlRegex.test(originalUrl)) {
     return res.status(400).json({ error: "invalid url" });
   }
 
+  // Generate short URL
   const shortUrl = shortUrlCounter++;
   urlDatabase[shortUrl] = originalUrl;
 
+  // Return JSON response with original_url and short_url properties
   return res.json({ original_url: originalUrl, short_url: shortUrl });
 });
 
+// Endpoint to redirect to original URL
 app.get("/api/shorturl/:short_url", function (req, res) {
   const shortUrl = req.params.short_url;
   const originalUrl = urlDatabase[shortUrl];
